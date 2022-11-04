81ab3a17a4eb9040046ca16dabb62ae886d6ece8
12ban <12ban@12mac.local>
2022-11-04 21:26:41 +0800

    funcition login

--- jobpit/settings.py
+++ jobpit/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'jobpit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('template'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
